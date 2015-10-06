d5b578447c1a96dee4226a43b21c717f817143d4
jeff seymour & quinn oaks <student@epicodus.com>
2015-10-05 19:00:47 -0700

    add function to select with color background

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,20 @@ $(document).ready(function() {
   $("button#meow").click(function() {
     $("ul#cat").prepend("<li>meow</li>");
     $("ul#dog").prepend("<li>bark bark</li>");
-  })
+
+    $("ul#cat").children("li").click(function() {
+      $(this).css("background-color", "green");
+    });
+  });
 
   $("button#bark").click(function() {
     $("ul#dog").prepend("<li>bark</li>");
     $("ul#cat").prepend("<li>meow meeooww</li>");
-  })
+
+    $("ul#dog").children("li").click(function() {
+      $(this).css("background-color", "yellow");
+    });
+  });
 
 
 
